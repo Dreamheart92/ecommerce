c1e7516660a91f0b22bfe1b153d580cc319b111e
Toni Rachev <tonirachev92@gmail.com>
2024-03-15 11:59:40 +0200

    added base header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
-function App() {
+import Header from "./components/Header.jsx";
 
+function App() {
+  return (
+    <main className="w-full flex justify-center">
+      <Header />
+    </main>
+  )
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+export default function Header() {
+    return (
+        <header className="flex justify-between w-[70%] bg-slate-400 p-2">
+            <span className="text-2xl">Logo</span>
+
+            <nav>
+                <ul className="flex gap-3 text-xl">
+                    <li>Menu</li>
+                    <li>Menu</li>
+                    <li>Menu</li>
+                    <li>Menu</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
